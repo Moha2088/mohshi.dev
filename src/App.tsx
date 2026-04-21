6adabbaaacadcb63d5bccf524055a85b87a09a73
Mohamed Shil <maxamed-14@outlook.dk>
2026-04-21 20:06:58 +0200

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <div className='main-div'>
+            <div className='name-div'>
+                <h1>
+                    Mohamed Shil
+                </h1>
+            </div>
+
+            <div className='description-div'>
+                <p>
+                    Full Stack Developer
+                </p>
+            </div>
+
+            <div className='links-div'>
+                <a className='social-link' href='https://github.com/Moha2088' target='_blank'>
+                    Github
+                </a>
+
+                <a  className='social-link' href="https://linkedin.com/in/mohamed-shil" target="_blank">
+                    LinkedIn
+                </a>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default App
