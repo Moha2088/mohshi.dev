2d04cfbdc1adb6eb3404aa7fed8b115da95974e6
Mohamed Shil <maxamed-14@outlook.dk>
2026-04-21 22:02:20 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,13 @@ function App() {
                 </p>
             </div>
 
+            <div className='paragraph-div'>
+                <p>
+                    I'm a Software Developer based in Odense, Denmark with experience in designing, developing and testing software applications.
+                    I have experience in both Frontend and Backend development in various programming languages and frameworks, which allows me to be flexible
+                </p>
+            </div>
+
             <div className='links-div'>
                 <a className='social-link' href='https://github.com/Moha2088' target='_blank'>
                     Github
