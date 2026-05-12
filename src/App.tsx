61a2272a4baf37449ccb0662993b6fa4f85c1109
Mohamed Shil <maxamed-14@outlook.dk>
2026-05-12 13:37:27 +0200

    add switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import { IconMoonFilled, IconSun } from '@tabler/icons-react'
+import { Switch } from './components/Switch'
 
 function App() {
   const [isDark, setIsDark] = useState(() => {
@@ -15,13 +16,19 @@ function App() {
 
   return (
     <>
-        <button 
+        {/* <button 
           className='theme-toggle'
           onClick={() => setIsDark(!isDark)}
           aria-label="Toggle light/dark mode"
         >
           {isDark ? <IconSun color='white' /> : <IconMoonFilled/>}
-        </button>
+        </button> */}
+
+        <Switch
+            isDark={isDark}
+            setIsDark={setIsDark}
+        />
+
         <div className='main-div'>
             <div className='name-div'>
                 <h1>
---
+++ src/components/Switch.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react"
+
+interface SwitchProps {
+    isDark: boolean
+    setIsDark: (val: boolean) => void
+}
+
+
+export function Switch(props: SwitchProps) {
+    const { isDark, setIsDark } = props
+
+    useEffect(() => {
+        document.documentElement.setAttribute("data-switch-to", isDark ? "dark" : "light")
+        console.log("Last saved was: " + document.documentElement.getAttribute("data-switch-to"))
+    }, [isDark])
+
+    return (
+        <div className="switch-root" onClick={() => {
+                if(isDark) {
+                    document.documentElement.setAttribute("data-switch-to", "light")
+                    setIsDark(false)
+                    return
+                }
+
+                document.documentElement.setAttribute("data-switch-to", "dark")
+                setIsDark(true)
+            }}
+        >
+            <div className="switch-thumb" /> 
+        </div>
+    )
+}
